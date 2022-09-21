52bba969f637d026a2bd822f444deceb71b3e6f2
Shadow Moon <88862050+Apolyonn@users.noreply.github.com>
2022-09-21 10:29:34 -0500

    Add files via upload
    Initial Commit. Barebones HTML

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Decks
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
