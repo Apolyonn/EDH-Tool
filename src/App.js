537182fb96eb2803abf40a64e79e0ea04ede8c3f
Shadow Moon <kitesblade@hotmail.com>
2022-10-11 10:55:18 -0500

    2.0

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import Deckcard from "./deckcard";
 
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-          <h1>
-            Your Decks
-          </h1>
-        </div>  
-      </header>
-    <div className="Card" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-      <Deckcard/>
-    </div>
-    </div>
+       <div className="Card">
+            <header className="App-header">
+                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+                    <h1>
+                    Your Decks
+                    </h1>
+                </div>  
+            </header>
+      <div className="Card" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <Deckcard/>
+        </div>
+</div>
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
